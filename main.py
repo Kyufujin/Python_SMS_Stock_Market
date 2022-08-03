5f00b0123850d14e84d6e5d1129bf6a9c2c768bf
Kyufujiin <suuben15@gmail.com>
2022-08-03 12:44:40 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#For more details check alpha vantage doc - https://www.alphavantage.co/documentation/#daily
+#Generated api key - UXFULFGK6YBTIC6S
+#DISCLAIMER - this key may be expired in the next few months
+import requests
+
+STOCK_NAME = "CDR"
+COMPANY_NAME = "CDPROJEKT"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API_KEY = "UXFULFGK6YBTIC6S"
+
