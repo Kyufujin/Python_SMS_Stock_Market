ff4ed5a6c1ff37adeeec4abc78d2a2208f507b90
Kyufujiin <suuben15@gmail.com>
2022-08-04 10:14:43 +0200

    - News Article, NEWS API

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #For more details check alpha vantage doc - https://www.alphavantage.co/documentation/#daily
 #Generated api key - UXFULFGK6YBTIC6S
+#Generated News Api key - bd27c4f031484cac91131226d99b368a
 #DISCLAIMER - this key may be expired in the next few months
 import requests
 
@@ -10,6 +11,7 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "UXFULFGK6YBTIC6S"
+NEWS_API_KEY = "bd27c4f031484cac91131226d99b368a"
 
 stock_params = {
     "function": "TIME_SERIES_DAILY",
@@ -30,4 +32,10 @@ difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_clo
 diff_percent = (difference / float(yesterday_closing_price)) * 100
 
 if diff_percent > 0.1:
-    print("Get News")
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+    three_articles = articles[:3]
