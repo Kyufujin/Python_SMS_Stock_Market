e431c3c522a277e3b7d4d2ab5c8d06fdfab73400
Kyufujiin <suuben15@gmail.com>
2022-08-04 14:53:14 +0200

    - Sending message to client Via TWILIO api

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 #Generated News Api key - bd27c4f031484cac91131226d99b368a
 #DISCLAIMER - this key may be expired in the next few months
 import requests
+from twilio.rest import Client
 
 STOCK_NAME = "CDR"
 COMPANY_NAME = "CDPROJEKT"
@@ -12,6 +13,8 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "UXFULFGK6YBTIC6S"
 NEWS_API_KEY = "bd27c4f031484cac91131226d99b368a"
+TWILIO_SID = "ACeba9d8510360adaa6b8495695243b291"
+TWILIO_AUTH_TOKEN = "c03a4ad72874c92ad240d1f6bc5c6893"
 
 stock_params = {
     "function": "TIME_SERIES_DAILY",
@@ -29,9 +32,15 @@ day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 
 difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
-diff_percent = (difference / float(yesterday_closing_price)) * 100
 
-if diff_percent > 0.1:
+up_down = None
+if difference > 0:
+    up_down = "✅"
+else:
+    up_down = "❌"
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
+
+if abs(diff_percent) > 0.1:
     news_params = {
         "apiKey": NEWS_API_KEY,
         "qInTitle": COMPANY_NAME,
@@ -39,3 +48,15 @@ if diff_percent > 0.1:
     news_response = requests.get(NEWS_ENDPOINT, params=news_params)
     articles = news_response.json()["articles"]
     three_articles = articles[:3]
+
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_="twilioAppNumberShouldBeHere",
+            to="YourNumber"
+        )
+#To send the message you need your own twilio number
+#After that you should put in "to" field your own number.
+#For more details check twilio SMS app doc - https://www.twilio.com/docs/sms/choose-the-right-phone-number-type
